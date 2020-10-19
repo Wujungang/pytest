3766afeabc14e7c30736ca5e3cb203cf78a475eb
wujg@calis.edu.cn <wujg@calis.edu.cn>
2020-10-19 14:35:08 +0800

    修改了代码

--- clsp/Test_App7.py
+++ clsp/Test_App7.py
@@ -1,13 +1,14 @@
 import pytest
 import requests
 import json
+import allure
 
 
 def return_test_data():
     return [(1, 2), (0, 3)]
 
 class Test_ABC:
-    @pytest.fixture()
+
     def setup_class(self):
         print("------->setup_class")
         return 999
@@ -16,36 +17,20 @@ class Test_ABC:
         print("------->teardown_class")
 
     # @pytest.mark.parametrize("a,b", return_test_data())  # 使用函数返回值的形式传入参数值
-    @pytest.fixture()
+
     def test_a(self):
-        headers = {
-            "Content-Type":"application/json",
-            "x-okapi-tenant":"l001736"
-        }
-        data = {
-            "username": "administrator",
-            "password": "letmein"
-        }
-        data = json.dumps(data)
-        res = requests.post("http://39.106.33.252/okapia/bl-users/login",data=data,headers = headers)
+        print(123456)
+        assert 1
 
-        assert len(res.headers["x-okapi-token"]) == 277
-        return res.headers["x-okapi-token"]
+    def test_b(self):
+        print(999)
+        assert 0
 
-    @pytest.fixture()
     def test_c(self):
-        return 456
+        print(123)
+        assert 1
 
 
-    def test_b(self,test_a,test_c,setup_class):
-        headers = {
-            "x-okapi-tenant":"l001736",
-            "x-okapi-token":test_a
-        }
-        res = requests.get("http://39.106.33.252/okapia/_/discovery/modules/mod-validatecode-0.0.1-SNAPSHOT")
-        print(res.text)
-        print(test_c)
-        print(setup_class)
 
 if __name__ == '__main__':
   pytest.main(["-s","Test_App7.py"])
